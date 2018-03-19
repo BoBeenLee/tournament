d48fd584da687c15264bec0251ab7329b469e472
bobinlee <cultist_tp@naver.com>
2018-03-19 17:31:09 +0900

    add readme, travis.

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-#@ tournament
+## tournament
 tournament of ideal type
 
 ### Features
 - ES6
+- babel
 - Sass
-- Javis
 - bundlesize
 - travis
 
+### Demo
+- https://public-tccpjjjycd.now.sh/
 
 ### Faker
 - https://randomuser.me
 
-
+### Theme
 - https://color.adobe.com/ko/Neutral-Blue-color-theme-22361/
--- src/index.js
+++ src/index.js
@@ -7,9 +7,3 @@ import { render as resultRender } from './pages/Result';
 
 reset();
 mainRender();
-console.log(global.state);
-
-// const add3 = partial((a, b) => a + b, 3);
-// const multiple = (a, b) => a * b;
-// console.log(compose(add3, multiple)(5, 3));
-// console.log(reducer([1, 2, 3, 4, 5], (res, num) => res + num, 5));
